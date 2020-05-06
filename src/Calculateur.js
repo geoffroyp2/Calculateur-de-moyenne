6db21106aff35f7e10c0041d7703bcb615fe0977
einsenhorn <einsenhorn@gmail.com>
2020-05-06 20:37:02 +0200

    wip

--- src/Calculateur.js
+++ src/Calculateur.js
@@ -8,6 +8,8 @@ const Calculateur = () => {
     const [filiere, changeFiliere] = useState(filieres[0]);
     const [obligatoire, changeObligatoire] = useState(filieres[0].obl[0]);
     const [specialite, changeSpecialite] = useState(filieres[0].spe[0]);
+    const [option1, changeOption1] = useState(matieres.OPS[0]);
+    const [option2, changeOption2] = useState(matieres.OPS[1]);
     const [notes, changeNotes] = useState({});
     const [avg, changeAvg] = useState(0);
 
@@ -21,43 +23,83 @@ const Calculateur = () => {
         changeFiliere(newFiliere[0]);
     }, [filieres]);
 
-    const onChangeObligatoire = useCallback((evt) => changeObligatoire(evt.target.value), []);
-    const onChangeSpecialite = useCallback((evt) => changeSpecialite(evt.target.value), []);
-    const onSubmit = useCallback((e) => {
-        e.preventDefault();
-
-        console.log(specialite, obligatoire);
+    const calculateMoyenne = () => {
         const moyenne = (
-            filiere.mat.reduce((acc, mat) => acc + mat.coef * (notes[mat.id] || 0), 0) /
+            filiere.mat.reduce((acc, mat) => {
+                const id = mat.id;
+
+                // exception du TPE
+                if (id === "TPE" && notes[id] <= 10) {
+                    return acc;
+                }
+
+                return acc + mat.coef * (notes[id] || 0);
+            }, 0)
+            /
             filiere.mat.reduce((acc, mat) => acc + mat.coef, 0)
         ).toFixed(2);
 
         changeAvg(moyenne);
+    };
+
+    const onChangeObligatoire = useCallback((evt) => {
+        changeObligatoire(evt.target.value);
+        changeNotes({});
+    }, []);
+    const onChangeSpecialite = useCallback((evt) => {
+        changeSpecialite(evt.target.value);
+        changeNotes({});
+    }, []);
+    const onChangeOption = useCallback((idx) => {
+        // afin d'eviter que la personne selectionne deux fois la meme options.
+        return (evt) => {
+            const value = evt.target.value;
+
+            if (idx === 1) {
+                if (value === option2) {
+                    changeOption2(option1);
+                }
+
+                changeOption1(value);
+            }
+            else {
+                if (value === option1) {
+                    changeOption1(option2);
+                }
+
+                changeOption2(value);
+            }
+        }
+    }, [option1, option2]);
+    const onSubmit = useCallback((e) => {
+        e.preventDefault();
+
+        calculateMoyenne();
     }, [notes, filiere.mat, specialite]);
 
     return (
         <div>
             <Select
                 className="filiere"
-                defaultValue={filiere.nom}
                 label="Filière : "
                 onChange={onChangeFiliere}
+                value={filiere.nom}
                 values={filieres}
                 optionKey="nom"
                 optionLabel="nom"
             />
             <Select
                 className="obligatoire"
-                defaultValue={obligatoire}
                 label="Enseignement obligatoire : "
                 onChange={onChangeObligatoire}
+                value={obligatoire}
                 values={filiere.obl}
             />
             <Select
                 className="specialite"
-                defaultValue={specialite}
                 label="Enseignement de spécialité : "
                 onChange={onChangeSpecialite}
+                value={specialite}
                 values={filiere.spe}
             />
 
@@ -78,9 +120,18 @@ const Calculateur = () => {
                                     {filiere.mat.filter((mat) => mat.an === "PREM").map((mat) => {
                                         return (
                                             <tr key={mat.id}>
-                                                <td>{matieres[mat.id]}</td>
-                                                <td>{mat.coef}</td>
-                                                <td><input type="number" min="0" max="20" onChange={(e) => changeNotes({...notes, [mat.id]: e.target.value})} /></td>
+                                                <td>
+                                                    {matieres[mat.id]}
+                                                </td>
+                                                <td>
+                                                    {mat.coef}
+                                                </td>
+                                                <td>
+                                                    <input type="number" min="0" max="20" onChange={(e) => {
+                                                        changeNotes({...notes, [mat.id]: e.target.value});
+                                                        calculateMoyenne();
+                                                    }} />
+                                                </td>
                                             </tr>
                                         )
                                     })}
@@ -101,17 +152,32 @@ const Calculateur = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {filiere.mat.filter((mat) => mat.an === "TERM").map((mat) => {
-                                        const coef = mat.id === specialite ? mat.coef + 2 : mat.coef;
+                                    {filiere
+                                        .mat
+                                        .filter((mat) => {
+                                            return mat.an === "TERM";
+                                        })
+                                        .map((mat) => {
+                                            const coef = mat.id === specialite ? mat.coef + 2 : mat.coef;
 
-                                        return (
-                                            <tr key={mat.id}>
-                                                <td>{matieres[mat.id]}</td>
-                                                <td>{coef}</td>
-                                                <td><input type="number" min="0" max="20" onChange={(e) => changeNotes({...notes, [mat.id]: e.target.value})} /></td>
-                                            </tr>
-                                        )
-                                    })}
+                                            return (
+                                                <tr key={mat.id}>
+                                                    <td>
+                                                        {matieres[mat.id]}
+                                                    </td>
+                                                    <td>
+                                                        {coef}
+                                                    </td>
+                                                    <td>
+                                                        <input type="number" min="0" max="20" onChange={(e) => {
+                                                            changeNotes({...notes, [mat.id]: e.target.value});
+                                                            calculateMoyenne();
+                                                        }} />
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
                                 </tbody>
                             </table>
                         </div>
@@ -119,7 +185,57 @@ const Calculateur = () => {
 
                     <div>
                         <div><b>Options</b></div>
-                        
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Matieres</th>
+                                    <th>Coefficient</th>
+                                    <th>Note</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                    <td>
+                                        <Select
+                                            className="options"
+                                            label="Option 1 : "
+                                            onChange={onChangeOption(1)}
+                                            value={option1}
+                                            values={matieres.OPS}
+                                        />
+                                    </td>
+                                    <td>
+                                        {filiere.mat.find((mat) => mat.id === "OPS1").coef}
+                                    </td>
+                                    <td>
+                                        <input type="number" min="0" max="20" onChange={(e) => {
+                                            changeNotes({...notes, OPS1: e.target.value});
+                                            calculateMoyenne();
+                                        }} />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <Select
+                                            className="options"
+                                            label="Option 2 : "
+                                            onChange={onChangeOption(2)}
+                                            value={option2}
+                                            values={matieres.OPS}
+                                        />
+                                    </td>
+                                    <td>
+                                        {filiere.mat.find((mat) => mat.id === "OPS2").coef}
+                                    </td>
+                                    <td>
+                                        <input type="number" min="0" max="20" onChange={(e) => {
+                                            changeNotes({...notes, OPS2: e.target.value});
+                                            calculateMoyenne();
+                                        }} />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
 
                     <div>
--- src/Select.js
+++ src/Select.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Select = ({className, defaultValue, label, onChange, values, optionKey = null, optionLabel = null}) => {
+const Select = ({className, label, onChange, value, values, optionKey = null, optionLabel = null}) => {
     return (
         <div className={className}>
             <label className={`${className}-label`}>{label}</label>
-            <select className={`${className}-select`} onChange={onChange} defaultValue={defaultValue}>
-                {values.map((val) => <option key={optionKey !== null ? val[optionKey] : val}>{optionLabel !== null ? val[optionLabel] : val}</option>)}
+            <select className={`${className}-select`} onChange={onChange} value={value}>
+                {values.map((val) => <option key={optionKey !== null ? val[optionKey] : val} >{optionLabel !== null ? val[optionLabel] : val}</option>)}
             </select>
         </div>
     )

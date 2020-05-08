b74e8814594a6cc2fe0239fab130546ad3bfb530
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2020-05-08 16:27:26 +0200

    version 0.1.loulou.5
    j'ai avancé :
    -- début du css,
    -- j'ai rajouté les subtilités dont on avait parlé (choix obligatoire/spé + calcul de la moyenne en prenant en compte tout ça)
    -- j'ai un peu réorganisé data.json (ça devrait être un peu plus cohérent :D)
    -- j'ai un peu condensé le code

--- src/Calculateur.js
+++ src/Calculateur.js
@@ -1,15 +1,34 @@
-import React, {useState, useCallback} from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Select from './Select';
 import data from './data.json';
 
+import Table from 'react-bootstrap/Table'
+
+/*
+A INSTALLER
+
+npm install react-bootstrap bootstrap
+
+TODO:
+-- Pour que ce soit plus lisible, on pourrait couper le code en sous-classes (notamment dans le tableau des matières)
+-- Ajouter des infos pop-up pour expliquer le calcul des matières bonus
+-- Ajouter des infos pour les mentions
+-- Ajouter les autres filières
+-- Avancer le CSS :
+        -- gérer le re-size automatique pour que ce soit moins dégueu
+        -- revoir les polices/tailles/couleurs de texte
+        -- revoir les fonds/encadrés, etc
+-- Vérifier les bugs et le calcul de la moyenne dans toutes les configurations de filières/obligatoire/spé
+*/
+
 const Calculateur = () => {
-    const {filieres, matieres} = data;
+    const { filieres, matieres } = data;
     const [filiere, changeFiliere] = useState(filieres[0]);
     const [obligatoire, changeObligatoire] = useState(filieres[0].obl[0]);
     const [specialite, changeSpecialite] = useState(filieres[0].spe[0]);
-    const [option1, changeOption1] = useState(matieres.OPS[0]);
-    const [option2, changeOption2] = useState(matieres.OPS[1]);
+    const [option1, changeOption1] = useState(filieres[0].OPT[0]);
+    const [option2, changeOption2] = useState(filieres[0].OPT[1]);
     const [notes, changeNotes] = useState({});
     const [avg, changeAvg] = useState(0);
 
@@ -21,40 +40,47 @@ const Calculateur = () => {
         }
 
         changeFiliere(newFiliere[0]);
+
+        // Pour tout reset quand on change de filliere (nécessaire pour l'affichage correct des select d'obligatoire/spé) 
+        // Faut peut être reset tous les autres states aussi pour pas polluer le calcul de la note finale
+        changeObligatoire(newFiliere[0].obl[0]);
+        changeSpecialite(newFiliere[0].spe[0]);
     }, [filieres]);
 
     const calculateMoyenne = () => {
         const moyenne = (
             filiere.mat.reduce((acc, mat) => {
-                const id = mat.id;
-
-                // exception du TPE
-                if (id === "TPE" && notes[id] <= 10) {
-                    return acc;
+                const isObl = filiere.obl.includes(mat.id);
+                if (mat.bonus) { // Pour les matières bonus (TPE & Options)
+                    return (notes[mat.id] || 0) <= 10 ? acc : acc + mat.coef * (notes[mat.id] - 10); //Seulement les notes au-dessus de 10
+                } else {
+                    return isObl && obligatoire !== mat.id ? acc : acc + mat.coef * (notes[mat.id] || 0); //Seulement la matière obligatoire choisie
                 }
-
-                return acc + mat.coef * (notes[id] || 0);
             }, 0)
             /
-            filiere.mat.reduce((acc, mat) => acc + mat.coef, 0)
+            filiere.mat.reduce((acc, mat) => {
+                const isObl = filiere.obl.includes(mat.id);
+                return mat.bonus || (isObl && obligatoire !== mat.id) ? acc : acc + mat.coef; // les coefs des matières bonus ne s'ajoutent pas au total
+            }, 0)
         ).toFixed(2);
 
         changeAvg(moyenne);
     };
 
     const onChangeObligatoire = useCallback((evt) => {
-        changeObligatoire(evt.target.value);
+        changeObligatoire(Object.entries(matieres).find(i => i[1] === evt.target.value)[0]); //Il y a surement plus simple ;D
         changeNotes({});
-    }, []);
+    }, [matieres]);
+
     const onChangeSpecialite = useCallback((evt) => {
-        changeSpecialite(evt.target.value);
+        changeSpecialite(Object.entries(matieres).find(i => i[1] === evt.target.value)[0]);
         changeNotes({});
-    }, []);
+    }, [matieres]);
+
     const onChangeOption = useCallback((idx) => {
         // afin d'eviter que la personne selectionne deux fois la meme options.
         return (evt) => {
             const value = evt.target.value;
-
             if (idx === 1) {
                 if (value === option2) {
                     changeOption2(option1);
@@ -71,6 +97,7 @@ const Calculateur = () => {
             }
         }
     }, [option1, option2]);
+
     const onSubmit = useCallback((e) => {
         e.preventDefault();
 
@@ -79,175 +106,112 @@ const Calculateur = () => {
 
     return (
         <div>
-            <Select
-                className="filiere"
-                label="Filière : "
-                onChange={onChangeFiliere}
-                value={filiere.nom}
-                values={filieres}
-                optionKey="nom"
-                optionLabel="nom"
-            />
-            <Select
-                className="obligatoire"
-                label="Enseignement obligatoire : "
-                onChange={onChangeObligatoire}
-                value={obligatoire}
-                values={filiere.obl}
-            />
-            <Select
-                className="specialite"
-                label="Enseignement de spécialité : "
-                onChange={onChangeSpecialite}
-                value={specialite}
-                values={filiere.spe}
-            />
+            <h1 className={`text-center`}>Calculateur de moyenne</h1>
+            <div className={`container border border-primary rounded shadow-lg p-3 m-3`}>
+                <div className={``}> {/* Je sépare le label du select custom pour pouvoir gérer la mise en page*/}
+                    <label className={`pr-3`}>Filière :</label>
+                    <Select
+                        className="" //class name du Select
+                        onChange={onChangeFiliere}
+                        value={filiere.nom}
+                        values={filieres}
+                        optionKey="nom"
+                        optionLabel="nom"
+                    />
+                </div>
+                {obligatoire ?
+                    <div className={``}>
+                        <label className={`pr-3`}>Enseignement obligatoire :</label>
+                        <Select
+                            className=""
+                            onChange={onChangeObligatoire}
+                            value={matieres[obligatoire]}
+                            values={filiere.obl.map(id => matieres[id])}
+                        />
+                    </div> :
+                    null
+                }
+                {specialite ?
+                    <div className={``}>
+                        <label className={`pr-3`}>Enseignement de spécialité : </label>
+                        <Select
+                            className=""
+                            onChange={onChangeSpecialite}
+                            value={matieres[specialite]}
+                            values={filiere.spe.concat([obligatoire]).map(id => matieres[id])} // Pour ajouter la matière obligatoire au choix de spé
+                        />
+                    </div> :
+                    null
+                }
+            </div>
 
-            <div>
+            <div className={`container border border-primary rounded shadow-lg p-3 m-3`}>
                 <form onSubmit={onSubmit}>
-                    <div>
-                        <div><b>Epreuves anticipées</b></div>
-                        <div>
-                            <table>
+                    <Table className={`table-bordered table-striped`} size={`sm`}>
+                        {[...Array(3)].map((elem, idx) =>
+                            <React.Fragment key={`tableau${idx}`}>
                                 <thead>
                                     <tr>
-                                        <th>Matieres</th>
-                                        <th>Coefficient</th>
-                                        <th>Note</th>
+                                        <th colSpan="3" className={`h4 font-weight-bold pb-2 pt-2`}>
+                                            {["Epreuves anticipées", "Epreuves Terminales", "Options"][idx]}
+                                        </th>
+                                    </tr>
+                                    <tr>
+                                        <th className={`h5 font-weight-normal`}>Matière</th>
+                                        <th className={`h5 font-weight-normal`}>Coefficient</th>
+                                        <th className={`h5 font-weight-normal`}>Note</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {filiere.mat.filter((mat) => mat.an === "PREM").map((mat) => {
-                                        return (
-                                            <tr key={mat.id}>
-                                                <td>
-                                                    {matieres[mat.id]}
-                                                </td>
-                                                <td>
-                                                    {mat.coef}
-                                                </td>
-                                                <td>
-                                                    <input type="number" min="0" max="20" onChange={(e) => {
-                                                        changeNotes({...notes, [mat.id]: e.target.value});
-                                                        calculateMoyenne();
-                                                    }} />
-                                                </td>
-                                            </tr>
-                                        )
-                                    })}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    
-                    <div>
-                        <div><b>Epreuves Terminales</b></div>
-                        <div>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Matieres</th>
-                                        <th>Coefficient</th>
-                                        <th>Note</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {filiere
-                                        .mat
-                                        .filter((mat) => {
-                                            return mat.an === "TERM";
-                                        })
-                                        .map((mat) => {
-                                            const coef = mat.id === specialite ? mat.coef + 2 : mat.coef;
-
+                                    {filiere.mat.filter((mat) => mat.an === ["PREM", "TERM", "OPT"][idx]).map((mat) => {
+                                        const coef = mat.id === specialite ? mat.coef + 2 : mat.coef;
+                                        const isObl = filiere.obl.includes(mat.id);
+                                        if (coef !== 0 && (!isObl || (isObl && obligatoire === mat.id))) {
                                             return (
                                                 <tr key={mat.id}>
-                                                    <td>
-                                                        {matieres[mat.id]}
+                                                    <td className={`pb-0 pt-1`}>
+                                                        <span className={`float-left pl-1 pr-1`}>{matieres[mat.id]}</span>
+                                                        {idx < 2 ?
+                                                            <span className={`float-right text-muted pl-1 pr-2`}>{mat.type}</span> :
+                                                            <span className={`pl-1`}>
+                                                                <Select
+                                                                    className="options"
+                                                                    onChange={onChangeOption(+mat.id[3])}
+                                                                    value={mat.id[3] === '1' ? option1 : option2}
+                                                                    values={filiere.OPT}
+                                                                />
+                                                            </span>
+                                                        }
                                                     </td>
-                                                    <td>
+                                                    <td className={`pb-0 pt-1`}>
                                                         {coef}
                                                     </td>
-                                                    <td>
+                                                    <td className={`pb-0 pt-1`}>
                                                         <input type="number" min="0" max="20" onChange={(e) => {
-                                                            changeNotes({...notes, [mat.id]: e.target.value});
+                                                            changeNotes({ ...notes, [mat.id]: e.target.value });
                                                             calculateMoyenne();
                                                         }} />
                                                     </td>
                                                 </tr>
                                             )
-                                        })
-                                    }
+                                        } else return null;
+                                    })}
                                 </tbody>
-                            </table>
-                        </div>
-                    </div>
-
-                    <div>
-                        <div><b>Options</b></div>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Matieres</th>
-                                    <th>Coefficient</th>
-                                    <th>Note</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                    <td>
-                                        <Select
-                                            className="options"
-                                            label="Option 1 : "
-                                            onChange={onChangeOption(1)}
-                                            value={option1}
-                                            values={matieres.OPS}
-                                        />
-                                    </td>
-                                    <td>
-                                        {filiere.mat.find((mat) => mat.id === "OPS1").coef}
-                                    </td>
-                                    <td>
-                                        <input type="number" min="0" max="20" onChange={(e) => {
-                                            changeNotes({...notes, OPS1: e.target.value});
-                                            calculateMoyenne();
-                                        }} />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <Select
-                                            className="options"
-                                            label="Option 2 : "
-                                            onChange={onChangeOption(2)}
-                                            value={option2}
-                                            values={matieres.OPS}
-                                        />
-                                    </td>
-                                    <td>
-                                        {filiere.mat.find((mat) => mat.id === "OPS2").coef}
-                                    </td>
-                                    <td>
-                                        <input type="number" min="0" max="20" onChange={(e) => {
-                                            changeNotes({...notes, OPS2: e.target.value});
-                                            calculateMoyenne();
-                                        }} />
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-
-                    <div>
-                        <button type="submit" onClick={onSubmit}>Submit</button>
+                            </React.Fragment >
+                        )}
+                    </Table>
+                    <div className={`pl-3 pb-3`}>
+                        <button type="submit" onClick={onSubmit} className={`btn-primary btn-sm shadow`}>Calculer</button>
                     </div>
                 </form>
-            </div>
 
-            <div>
-                <div>Moyenne: {avg > 0 && avg}</div>
+                <div className={`alert ${avg > 10 ? `alert-success` : `alert-danger`} p-2`}>
+                    <span className={`pr-3`}> Moyenne :</span>
+                    <span>{avg} / 20</span>
+                    <div>Plus d'infos <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">ici</a></div>
+                </div>
             </div>
-        </div>
+        </div >
     );
 };
 
--- src/Select.js
+++ src/Select.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
-const Select = ({className, label, onChange, value, values, optionKey = null, optionLabel = null}) => {
+// import Table from 'react-bootstrap/Table'
+
+const Select = ({ className, onChange, value, values, optionKey = null, optionLabel = null }) => {
     return (
-        <div className={className}>
-            <label className={`${className}-label`}>{label}</label>
-            <select className={`${className}-select`} onChange={onChange} value={value}>
-                {values.map((val) => <option key={optionKey !== null ? val[optionKey] : val} >{optionLabel !== null ? val[optionLabel] : val}</option>)}
-            </select>
-        </div>
+        <select className={`${className}`} onChange={onChange} value={value}>
+            {values.map((val) => <option key={optionKey !== null ? val[optionKey] : val} >{optionLabel !== null ? val[optionLabel] : val}</option>)}
+        </select>
     )
 };
 

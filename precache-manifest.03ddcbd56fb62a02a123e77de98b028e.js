c4fece72299d2cdc15258d7ee59c1eff3d6d75f8
geoffroyp2 <51380977+geoffroyp2@users.noreply.github.com>
2020-05-15 00:24:02 +0200

    Updates

---
+++ precache-manifest.03ddcbd56fb62a02a123e77de98b028e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd490c095653500c5083e9838a977a80",
+    "url": "/Calculateur-de-moyenne/index.html"
+  },
+  {
+    "revision": "93554cb14f57a51404fb",
+    "url": "/Calculateur-de-moyenne/static/css/main.33e5d89c.chunk.css"
+  },
+  {
+    "revision": "9c1dcd096cc06a8df169",
+    "url": "/Calculateur-de-moyenne/static/js/2.6d4c2639.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Calculateur-de-moyenne/static/js/2.6d4c2639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93554cb14f57a51404fb",
+    "url": "/Calculateur-de-moyenne/static/js/main.712bbaf8.chunk.js"
+  },
+  {
+    "revision": "530863ca7144cb3f3ad7",
+    "url": "/Calculateur-de-moyenne/static/js/runtime-main.77e42ba2.js"
+  }
+]);
